1eaf670cc955bdb691ba8dc9c4aa76ddf939066a
Diamur <neelovdima@gmail.com>
2024-04-12 15:10:04 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_X = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Y = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_Sec = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_Pos_X = new System.Windows.Forms.Label();
+            this.label_Pos_Y = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(405, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_X
+            // 
+            this.textBox_X.Location = new System.Drawing.Point(46, 24);
+            this.textBox_X.Name = "textBox_X";
+            this.textBox_X.Size = new System.Drawing.Size(45, 20);
+            this.textBox_X.TabIndex = 1;
+            this.textBox_X.Text = "500";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y";
+            // 
+            // textBox_Y
+            // 
+            this.textBox_Y.Location = new System.Drawing.Point(46, 60);
+            this.textBox_Y.Name = "textBox_Y";
+            this.textBox_Y.Size = new System.Drawing.Size(45, 20);
+            this.textBox_Y.TabIndex = 4;
+            this.textBox_Y.Text = "500";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_Y);
+            this.groupBox1.Controls.Add(this.textBox_X);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(183, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Установока координат мыши";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox_Sec);
+            this.groupBox2.Location = new System.Drawing.Point(318, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(96, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Частота клика";
+            // 
+            // textBox_Sec
+            // 
+            this.textBox_Sec.Location = new System.Drawing.Point(9, 44);
+            this.textBox_Sec.Name = "textBox_Sec";
+            this.textBox_Sec.Size = new System.Drawing.Size(45, 20);
+            this.textBox_Sec.TabIndex = 4;
+            this.textBox_Sec.Text = "1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label_Pos_Y);
+            this.groupBox3.Controls.Add(this.label_Pos_X);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(201, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(111, 100);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Позиция мышки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Y";
+            // 
+            // label_Pos_X
+            // 
+            this.label_Pos_X.AutoSize = true;
+            this.label_Pos_X.Location = new System.Drawing.Point(46, 27);
+            this.label_Pos_X.Name = "label_Pos_X";
+            this.label_Pos_X.Size = new System.Drawing.Size(14, 13);
+            this.label_Pos_X.TabIndex = 4;
+            this.label_Pos_X.Text = "X";
+            // 
+            // label_Pos_Y
+            // 
+            this.label_Pos_Y.AutoSize = true;
+            this.label_Pos_Y.Location = new System.Drawing.Point(46, 60);
+            this.label_Pos_Y.Name = "label_Pos_Y";
+            this.label_Pos_Y.Size = new System.Drawing.Size(14, 13);
+            this.label_Pos_Y.TabIndex = 5;
+            this.label_Pos_Y.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "sec";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 177);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Clicker";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_X;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Y;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_Sec;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label_Pos_Y;
+        private System.Windows.Forms.Label label_Pos_X;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        // [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        // [return: MarshalAs(UnmanagedType.Bool)]
+        // private static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("User32.dll")]
+        static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
+        private bool status = false;
+
+        //для удобства использования создаем перечисление с необходимыми флагами (константами), которые определяют действия мыши: 
+        [Flags]
+        enum MouseFlags
+        {
+            Move        = 0x0001, 
+            LeftDown    = 0x0002, 
+            LeftUp      = 0x0004, 
+            RightDown   = 0x0008,
+            RightUp     = 0x0010, 
+            Absolute    = 0x8000
+        };
+        enum Status
+        {
+            Stop = 0,
+            Start= 1
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            TopMost = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+
+
+            if (status)
+            {
+                timer1.Enabled = false;
+                status = false;
+                button1.Text = "Пуск";
+            }
+            else
+            {
+                int sec = Convert.ToInt32(textBox_Sec.Text.ToString());
+                timer1.Interval = 1000 * sec;
+                timer1.Enabled = true; 
+                status = true;
+                button1.Text = "Стоп";
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_Pos_X.Text = Cursor.Position.X.ToString();
+            label_Pos_Y.Text = Cursor.Position.X.ToString();
+
+            if (button1.Text == "")
+            {
+                button1.Text = "Стоп";
+            }
+            else
+            {
+                button1.Text = "";
+            }
+
+            int x = Convert.ToInt32(textBox_X.Text.ToString());
+            int y = Convert.ToInt32(textBox_Y.Text.ToString());
+
+            // mouse_event(MouseFlags.Absolute | MouseFlags.Move, x, y, 0, UIntPtr.Zero);
+            // клик левой кнопки мыши
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftDown, x, y, 0, UIntPtr.Zero);
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftUp, x, y, 0, UIntPtr.Zero);
+            
+            // клик правой кнопки мыши
+            //mouse_event(MouseFlags.Absolute | MouseFlags.RightUp, x, y, 0, UIntPtr.Zero);
+            //mouse_event(MouseFlags.Absolute | MouseFlags.RightDown, x, y, 0, UIntPtr.Zero);
+
+        }
+    }
+}
